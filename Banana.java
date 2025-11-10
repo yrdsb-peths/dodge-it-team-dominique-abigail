9efbdb14ade2c5c0e2bc000f8f2dbe48d0e4ce19
doMOBSTERnique <348787912@gapps.yrdsb.ca>
2025-11-10 14:28:35 -0500

    added sad face

--- Banana.java
+++ Banana.java
@@ -13,6 +13,9 @@ public class Banana extends Actor
         
         if(isTouching(Hero.class))
         {
+            SadFace sadFace = new SadFace();
+            getWorld().addObject(sadFace, 300, 200);
+            
             getWorld().removeObject(this);
         }
     }
